1efa86a5d86fec9455762889e8f6a9854c49f331
elenachaplygina <antonenko-elena@inbox.ru>
2023-04-13 21:02:30 +0300

    fix locale star

--- src/test/java/ui/TestBaseUI.java
+++ src/test/java/ui/TestBaseUI.java
@@ -1,9 +1,12 @@
 package ui;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import drivers.WebDriverProvider;
 import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import ui.page.Page;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -19,6 +22,11 @@ public class TestBaseUI {
         open("https://simplewine.ru/");
         $("[data-autotest-target-id=age-popup-btn]").click();
         $("[data-autotest-target-id=city-popup-city-close]").click();
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
